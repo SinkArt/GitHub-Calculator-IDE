774a706ea76dacab6ed0bb030ccc7114aecd9925
a.sinkevychus <sinkevichus@gmail.com>
2025-09-24 21:18:23 +0300

    Update app info

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,7 +2,7 @@ package app;
 
 public class Main {
 
-    public static void main (String[] args){
-        System.out.println("My git app.");
+    public static void main(String[] args) {
+        System.out.println("My git app. Super!");
     }
 }
